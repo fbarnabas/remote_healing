d153b31da349deb16bdfbee27f93ae9e70b02d44
fbarnabas <73483641+fbarnabas@users.noreply.github.com>
2021-06-25 19:24:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 # document what we're doing in-line with the actual commands
 # Note that a blank line (commented as "defualt" will send a empty
 # line terminated with a newline to take the fdisk default.
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda-á
   n # new partition
   p # primary partition
   3 # partition number 3
