7dd187e8bf36f7c9c97da55ecdc544302da3c36b
fbarnabas <73483641+fbarnabas@users.noreply.github.com>
2021-06-26 14:07:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,13 +14,19 @@ touch /mnt/sda5/tce/mydata.tgz
 tce-load -wi grub2-multi.tcz
 sudo grub-install --boot-directory=/mnt/sda5/boot /dev/sda
 cat >> /mnt/sda1/boot/grub/grub.cfg <<EOF
+menuentry "Windows 10" {
+	insmod chain
+	insmod ntfs
+	set root=(hd0,1)
+	chainloader +1
+}
 menuentry "Tiny Core Linux" {
 set root="(hd0,msdos5)"
 linux /tce/boot/vmlinuz
 initrd /tce/boot/core.gz
 }
 EOF
-wget https://raw.githubusercontent.com/fbarnabas/remote_healing/main/install1.sh --no-check-certificate
+wget https://raw.githubusercontent.com/fbarnabas/remote_healing/main/install2.sh --no-check-certificate
 chmod +x ./install1.sh
 ./install1.sh
 
