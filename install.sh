4925ba896531b2cdd10b3e51a4779205e36a7883
fbarnabas <73483641+fbarnabas@users.noreply.github.com>
2021-06-26 13:17:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 mkfs.ext4 /dev/sda3
 mkfs.ext4 /dev/sda5
+cat >> /etc/fstab <<EOF
+/dev/sda5 /mnt/sda5 ext4
+/dev/sda3 /mnt/sda3 ext4
+EOF
+mkdir /mnt/sda5
+mount /dev/sda5
+mkdir /mnt/sda5/boot
+cp -p /mnt/sr0/boot/* /mnt/hda5/boot
+mkdir /mnt/sda5/tce
+touch /mnt/hda5/tce/mydata.tgz
+tce-load -wi grub2-multi.tcz
+grub-install /dev/sda
+
+
 
