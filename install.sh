8d60886d01f675d5a81fbee386f199c1c13cf464
fbarnabas <73483641+fbarnabas@users.noreply.github.com>
2021-06-27 10:27:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cat >> /etc/fstab <<EOF
 /dev/sda3 /mnt/sda3 ext4
 EOF
 mkdir /mnt/sda5
-mount /dev/sda5
+mount -t ext4 /dev/sda5 /mnt/sda5
 mkdir -p /mnt/sda5/boot
 cp -p /mnt/sr0/boot/* /mnt/sda5/tce/boot
 mkdir -p /mnt/sda5/tce
