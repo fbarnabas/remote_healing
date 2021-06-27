3b76fa71680e34ada5a7b65f2526e5bf279677c8
fbarnabas <73483641+fbarnabas@users.noreply.github.com>
2021-06-27 11:25:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 mkfs.ext4 /dev/sda3
 mkfs.ext4 /dev/sda5
-cat >> /etc/fstab <<EOF
-/dev/sda5 /mnt/sda5 ext4
-/dev/sda3 /mnt/sda3 ext4
-EOF
+#cat >> /etc/fstab <<EOF
+#/dev/sda5 /mnt/sda5 ext4
+#/dev/sda3 /mnt/sda3 ext4
+#EOF
 mkdir /mnt/sda5
 sudo mount -t ext4 /dev/sda5 /mnt/sda5
 sudo mkdir -p /mnt/sda5/boot
-cp -p /mnt/sr0/boot/* /mnt/sda5/boot
+cp -p -r /mnt/sr0/boot/* /mnt/sda5/boot
 sudo mkdir -p /mnt/sda5/tce
+cp -p -r /mnt/sr0/cde/* /mnt/sda5/tce
 touch /mnt/sda5/tce/mydata.tgz
 tce-load -wi grub2-multi.tcz
 sudo grub-install --boot-directory=/mnt/sda5/boot /dev/sda
@@ -23,7 +24,7 @@ menuentry "Windows 10" {
 menuentry "Tiny Core Linux" {
 set root=(hd0,5)
 linux /tce/boot/vmlinuz
-initrd /tce/boot/core.gz
+initrd /boot/core.gz
 }
 EOF
 sudo mv ./grub.cfg /mnt/sda5/boot/grub/grub.cfg
