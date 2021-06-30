0b24c0fd0b459cf4fc46a8fb446a6e90656c2f4e
fbarnabas <73483641+fbarnabas@users.noreply.github.com>
2021-06-30 20:46:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
+#Formatting sda3 and sda5
 yes | mkfs.ext4 /dev/sda3
 yes | mkfs.ext4 /dev/sda5
-#cat >> /etc/fstab <<EOF
-#/dev/sda5 /mnt/sda5 ext4
-#/dev/sda3 /mnt/sda3 ext4
-#EOF
 mkdir /mnt/sda5
+#Mounting sda5
 sudo mount -t ext4 /dev/sda5 /mnt/sda5
 sudo mkdir -p /mnt/sda5/boot
 sudo cp -p -r /mnt/sr0/boot/* /mnt/sda5/boot
 sudo mkdir -p /mnt/sda5/tce
 sudo cp -p -r /mnt/sr0/cde/* /mnt/sda5/tce
 sudo touch /mnt/sda5/tce/mydata.tgz
+
+#Installing the Grub bootloader
 tce-load -wi grub2-multi.tcz
 tce-load -wi nano.tcz
 sudo grub-install --boot-directory=/mnt/sda5/boot /dev/sda
@@ -25,19 +25,16 @@ menuentry "Windows 10" {
 menuentry "Tiny Core Linux" {
 set root=(hd0,5)
 linux /boot/vmlinuz
-initrd /boot/core.gz
+initrd /boot/core.gz noautologin
 }
 default=2
 timeout=4
 EOF
 sudo mv ./grub.cfg /mnt/sda5/boot/grub/grub.cfg
-#cat > /opt/.backup_device <<EOF
-#/mnt/sda5/tce/optional
-#EOF
 #wget https://raw.githubusercontent.com/fbarnabas/remote_healing/main/install2.sh --no-check-certificate
-#chmod +x ./install2.sh
 sudo chown tc -R /mnt/sda5/tce
 sudo chmod 775 -R /mnt/sda5/tce
+#Installing ssh server
 tce-load -wi openssh.tcz
 sudo cp /usr/local/etc/ssh/sshd_config.orig /usr/local/etc/ssh/sshd_config
 sudo /usr/local/etc/init.d/openssh start
@@ -47,17 +44,28 @@ sudo /usr/local/etc/init.d/openssh start
 EOF
 chmod +x bootlocal.sh
 sudo mv bootlocal.sh /opt/bootlocal.sh
+
+#Following directories will be backed up before every shutdown and reboot
 cat >> /opt/.filetool.lst <<EOF
 usr/local/etc/ssh
 etc/shadow
 EOF
 # sudo passwd tc
+
+#Installing ntfsprogs
 tce-load -wi ntfsprogs.tcz
+
+#Mounting sda3
 mkdir /mnt/sda3
 sudo mount -t ext4 /dev/sda3 /mnt/sda3
 sudo chown -R tc /mnt/sda3
 mkdir -p /mnt/sda3/images
+
+#Backing up the windows paertitions sda1 and sda2
 ntfsclone --save-image --output /mnt/sda3/images/win10-sda1.img /dev/sda1
+#ntfsclone --save-image --output /mnt/sda3/images/win10-sda2.img /dev/sda2
+
+#Copying TinyCore Linux extensions to disk
 cd /mnt/sda5/tce/optional
 #sudo cp /tmp/tce/optional/nano.tcz .
 #sudo cp /tmp/tce/optional/nano.tcz.dep .
@@ -74,6 +82,8 @@ cat >> /mnt/sda5/tce/onboot.lst <<EOF
 openssh
 ntfsprogs
 EOF
+
+#Downloading restorewin.sh
+curl -O https://raw.githubusercontent.com/fbarnabas/remote_healing/main/restorewin.sh
+chmod +x restorewin.sh
 filetool.sh -b
-
-
